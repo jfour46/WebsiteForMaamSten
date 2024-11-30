30a24c7e7e13166f76f368589abfecd026d7b962
jfour46 <jeffarabit6@gmail.com>
2024-11-30 13:12:27 +0800

    Add files via upload

---
+++ WebsiteforMaamSten/app.js
@@ -0,0 +1,17 @@
+const searchicon1 = document.querySelector('#seachicon1');
+const srchicon1 = document.querySelector('#srchicon1');
+const search1 = document.querySelector('#searchinput1');
+
+searchicon1.addEventListener('click', function(){
+    search1.style.diplay = 'flex';
+    searchicon1.style.diplay = 'none';
+})
+
+const searchicon2 = document.querySelector('#seachicon1');
+const srchicon2 = document.querySelector('#srchicon1');
+const search2 = document.querySelector('#searchinput1');
+
+searchicon2.addEventListener('click', function(){
+    search2.style.diplay = 'flex';
+    searchicon2.style.diplay = 'none';
+})
